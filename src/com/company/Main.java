be46fbeadfec2aa24d66f9abef88683c89547a99
finalWork <sashaneiv01@yandex.ru>
2022-05-22 21:02:00 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String alphabet = "a b c d";
+        String user = "Alex|qwerty@mail.ru";
+
+        String[] splitUser = user.split("[\\|.@]");
+        String[] splitAlphabet = alphabet.split(" ", 2);
+
+        for (String word : splitAlphabet) {
+            System.out.println(word);
+        }
+
+        for (String word : splitUser) {
+            System.out.println(word);
+        }
+    }
+}
